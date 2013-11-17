72d72847fe48752c18a4de7310f4487db2445fc7
cg-cnu <sreenivas9alapati@gmail.com>
2013-11-17 20:45:57 +0530

    fixed variable bugs

--- retopo_workflow.py
+++ retopo_workflow.py
@@ -1,77 +1,76 @@
 '''
-Automate the below workflow
+Automates the workflow in the tutorial below...
 http://cgcookie.com/blender/2011/08/08/retopology-with-the-bsurfaces-add-on/
-Install the addon and in 3d view press space and type retopo workflow.
+make the lidar active and in 3d view press space and type retopo workflow.
+Creates the setup for the active / last selected object.
 '''
 
 bl_info = {
-    "name": "Retopo Workflow",
-    "description": " setup and automate the retopo workflow ",
-    "author": "Sreenivas Alapati",
-    "version": (1, 0),
-    "blender": (2, 65, 0),
-    "category": "Mesh"}
+	"name": "Retopo Workflow",
+	"description": " setup and automate the retopo workflow ",
+	"author": "Sreenivas Alapati",
+	"version": (1, 0),
+	"blender": (2, 65, 0),
+	"category": "Mesh"}
 
 import bpy 
 
-C = bpy.context
-D = bpy.data
-O = bpy.ops
+def main(self, context):
+	C = bpy.context
+	D = bpy.data
+	O = bpy.ops
+	name = C.active_object.name
+	if D.objects[name].type != 'MESH':
+		print ('You have selected a non mesh object')
+	else:
+		# add mesh
+		O.mesh.primitive_plane_add()
+		# set its positon to lidar's origin 
+		C.object.location.xyz = bpy.data.objects[name].location.xyz
+		# rename the object as name_retopo
+		C.active_object.name = name + '_retopo'
+		# get the name of the object as retopo         
+		retopo = C.active_object.name    
+		
+		# apply shrinkwrap and rename
+		O.object.modifier_add(type='SHRINKWRAP')      
+		D.objects[retopo].modifiers['Shrinkwrap'].target = bpy.data.objects[name]
+		D.objects[retopo].modifiers['Shrinkwrap'].name = name + '_Shrinkwrap'
+		
+		# delete the verts    
+		O.object.editmode_toggle()
+		O.mesh.delete(type='VERT')
+		O.object.editmode_toggle()
 
-def main(context):
-    ## check if there is a selected object.
-    if C.selected_objects == []:
-        print ('Please select a mesh object')
-    else:
-        name = C.active_object.name
-        if D.objects[name].type != 'MESH':
-            print ('You have selected a non mesh object')
-        else:
-            # add mesh
-            O.mesh.primitive_plane_add()
-            # set its positon to lidar's origin 
-            C.object.location.xyz = bpy.data.objects[name].location.xyz
-            # rename the object as name_retopo
-            C.active_object.name = name + '_retopo'
-            # get the name of the object as retopo         
-            retopo = C.active_object.name    
-            
-            # apply shrinkwrap and rename
-            O.object.modifier_add(type='SHRINKWRAP')      
-            D.objects[retopo].modifiers['Shrinkwrap'].target = bpy.data.objects[name]
-            D.objects[retopo].modifiers['Shrinkwrap'].name = name + '_Shrinkwrap'
-            
-            # delete the verts    
-            O.object.editmode_toggle()
-            O.mesh.delete(type='VERT')
-            O.object.editmode_toggle()
-
-            # create a new grease pencil layer and rename it
-            O.gpencil.data_add()
-            O.gpencil.layer_add()        
-            D.objects[name + '_retopo'].grease_pencil.name = name + '_gpencil'
-            # set the layer mode to surface
-            D.grease_pencil[name + '_gpencil'].draw_mode = 'SURFACE'
+		# create a new grease pencil layer and rename it
+		O.gpencil.data_add()
+		O.gpencil.layer_add()        
+		D.objects[name + '_retopo'].grease_pencil.name = name + '_gpencil'
+		# set the layer mode to surface
+		D.grease_pencil[name + '_gpencil'].draw_mode = 'SURFACE'
+		
+		self.report({'INFO'}, " Created setup for retopo ")
+	return
 
 class RetopoWorkflow(bpy.types.Operator):
-    """ sets the object ready for retopo """
-    bl_idname = "object.retopo_workflow"
-    bl_label = "Retopo Workflow"
-    bl_options = {'REGISTER', 'UNDO'}
+	""" sets the object ready for retopo """
+	bl_idname = "object.retopo_workflow"
+	bl_label = "Retopo Workflow"
+	bl_options = {'REGISTER', 'UNDO'}
 
-    @classmethod
-    def poll(cls, context):
-        return context.active_object is not None
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None
 
-    def execute(self, context):
-        main(context)
-        return {'FINISHED'}
+	def execute(self, context):
+		main(self, context)
+		return {'FINISHED'}
 
 def register():
-    bpy.utils.register_class(RetopoWorkflow)
+	bpy.utils.register_class(RetopoWorkflow)
 
 def unregister():
-    bpy.utils.unregister_class(RetopoWorkflow) 
+	bpy.utils.unregister_class(RetopoWorkflow) 
 
 if __name__ == "__main__":
-    register()
+	register()
