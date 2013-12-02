377d1dff76455f42c5c64897f416adb4ea87d98f
sreenivas alapati <sreenivas9alapati@gmail.com>
2013-12-02 15:36:25 +0530

    Create Xmisc.py

---
+++ Xmisc.py
@@ -0,0 +1,29 @@
+# LIDAR SPLITTING:
+
+cut heavy duty lidar into parts based on... 
+# automatic
+# manually based on a user given mesh object
+# based on the view frustum
+
+# LIDAR DECIMATION:
+  
+GET SOME APPROACHES
+
+
+# IMPORTING SURVEY DATA:
+
+# import the survey data and create a base mesh based on that
+
+
+# AUTOMATIC UPDATE OF THE OBJ OBJECT ON DISK
+
+# IMPORTING STM FILES 
+# GETTING THE GROUPS WORKING
+
+
+# GETTING PROPER HIDDENLINE MODE IN BLENDER
+
+# EDGE LOOPS:
+  LOOPS HAS TO FOLLOW THE TEXTURE AND THE SURFACE
+  PROPER BREAKUP OF THE FACES - USEFUL FOR ALIGNING
+  
